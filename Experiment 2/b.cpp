31e0f8f3bb9771cdfc1d0f3188182fec0769d2ef
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2023-10-06 22:14:30 +0530

    I really don't remember what I did with this file

--- Experiment 2/b.cpp
+++ Experiment 2/b.cpp
@@ -21,27 +21,8 @@ int main(){
 	int x, y, s;
 	cout << "Enter two numbers: ";
 	cin >> x >> y;
-	cout <<"Enter your operation\n";
-	cout << "1. Addition\n";
-	cout << "2. Subtraction\n";
-	cout << "3. Multiplication\n";
-	cout << "4. Division\n";
-	cin >> s;
-	switch (s){
-		case 1:
-			cout << "The sum of " << x << " and " << y << " is "<< sum(x, y) << endl;
-			break;
-		case 2:
-			cout << "The difference of " << x << " and " << y << " is "<< diff(x, y) << endl;
-			break;
-		case 3:
-			cout << "The product of " << x << " and " << y << " is "<< mult(x, y) << endl;
-			break;
-		case 4:
-			cout << "The quotient of " << x << " and " << y << " is "<< divi(x, y) << endl;
-			break;
-		default:
-			cout << "Invalid input\n";
-	}
-	return 0;
+	cout << "The sum of " << x << " and " << y << " is "<< sum(x, y) << endl;
+	cout << "The difference of " << x << " and " << y << " is "<< diff(x, y) << endl;
+	cout << "The product of " << x << " and " << y << " is "<< mult(x, y) << endl;
+	cout << "The quotient of " << x << " and " << y << " is "<< divi(x, y) << endl;
 }
